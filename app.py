ca88b7dac8114a838d969ef1731dba7033edfef8
Sneha Reddy <snehareddy0151@gmail.com>
2026-07-15 11:02:08 +0530

    Demo pipeline

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("Hello GitHub Actions!")
+print("=" * 40)
+print("WELCOME TO GITHUB ACTIONS")
+print("=" * 40)
+
+print("Step 1 : Starting Application")
+print("Step 2 : Performing Calculation")
+
+a = 25
+b = 15
+
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} = {a-b}")
+print(f"{a} * {b} = {a*b}")
+print(f"{a} / {b} = {a/b}")
+
+print("Application Finished Successfully")
